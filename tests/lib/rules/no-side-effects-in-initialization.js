62ca7d23263df5961304a4a6d2fecad58a26a3cb
Lukas Taegert <lukas.taegert@tngtech.com>
2017-06-25 14:50:15 +0200

    First working test

--- tests/lib/rules/no-side-effects-in-initialization.js
+++ tests/lib/rules/no-side-effects-in-initialization.js
@@ -1,6 +1,4 @@
-'use strict'
-
-var rule = require('../../../lib/rules/no-side-effects-in-initialization'),
+const rule = require('../../../lib/rules/no-side-effects-in-initialization'),
   RuleTester = require('eslint').RuleTester
 
 RuleTester.setDefaultConfig({
@@ -10,20 +8,30 @@ RuleTester.setDefaultConfig({
   }
 });
 
-var ruleTester = new RuleTester()
+const ruleTester = new RuleTester()
 ruleTester.run('no-side-effects-in-initialization', rule, {
 
   valid: [
-    'const x = 1;const y = 2'
+    'const x = 1',
+    `const x = 1
+        x = 2`,
+    'export const x = {}'
   ],
 
   invalid: [
-    // {
-    //   code: 'x = 1',
-    //   errors: [{
-    //     message: "Stuff",
-    //     type: "ExpressionStatement"
-    //   }]
-    // }
+    {
+      code: 'x = 1',
+      errors: [{
+        message: "Initialization code should not have side effects",
+        type: "Identifier"
+      }]
+    },
+    {
+      code: 'export const x = Object.freeze({})',
+      errors: [{
+        message: "Initialization code should not have side effects",
+        type: "Identifier"
+      }]
+    }
   ]
 })
