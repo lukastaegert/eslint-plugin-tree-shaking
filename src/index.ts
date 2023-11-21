574e21f11e31e87b667a5699aff909f8472e88b1
Rongjian Zhang <pd4d10@gmail.com>
2023-11-21 22:30:21 +0800

    refactor: improve types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+import { Rule } from "eslint";
 import { noSideEffectsInInitialization } from "./rules/no-side-effects-in-initialization";
 
-export const rules = {
+export const rules: Record<string, Rule.RuleModule> = {
   "no-side-effects-in-initialization": noSideEffectsInInitialization,
 };
